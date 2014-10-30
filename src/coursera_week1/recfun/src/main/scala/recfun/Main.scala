c690e438430203798805eb4839cc923606649aa1
Claire Jaja <cjaja@sdl.com>
2014-10-29 17:18:47 -0700

    Add solution for assignment 1 exercise 2 and incomplete solution for exercise 3.

--- src/coursera_week1/recfun/src/main/scala/recfun/Main.scala
+++ src/coursera_week1/recfun/src/main/scala/recfun/Main.scala
@@ -21,10 +21,37 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    def countParentheses(chars: List[Char], x: Int): Boolean =
+      // if value is 0, return true
+      if (chars.isEmpty) x == 0
+      // scan - on every open parentheses, +1, on every close parentheses -1
+      else
+        if (chars.head == '(') countParentheses(chars.tail,x+1)
+        else if (chars.head == ')' && x > 0) countParentheses(chars.tail,x-1)
+        else if (chars.head == ')' && x <= 0) false
+        else countParentheses(chars.tail,x)
+
+    countParentheses(chars:List[Char],0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =  {
+
+    def loop(money: Int, coins: List[Int], acc: Int): Int =
+      if (coins.isEmpty) 0
+      else if (money == 0) 0
+      else { // coins exist, money > 0
+        // filter for coins less than money
+        def smallerCoins = coins.filter(x => x < money)
+        if (coins.contains(money)) acc+1+smallerCoins.map(x => loop(money-x,coins,acc)).sum
+        else acc+smallerCoins.map(x => loop(money-x,coins,acc)).sum
+        //acc + coins.filter(x => x < money).map(y => loop(money-y,coins,acc)).sum
+      }
+
+    loop(money,coins,0)
+  }
 }
